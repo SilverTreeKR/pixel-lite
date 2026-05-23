4b4f5b467163aaa491986b08517ce04ef9230b91
gonghan916 <iopiop4966@gmail.com>
2026-05-23 19:26:58 +0900

    Add: 헤더 추가

---
+++ PixelLite/PixelLite/header/UI.h
@@ -0,0 +1,13 @@
+#ifndef UI_H
+#define UI_H
+#define MENU_SIZE 8
+#define UP 1
+#define DOWN 2
+#define ENTER 3
+
+extern char* menu[MENU_SIZE];
+
+void displayMenu(int select);
+
+int getKey();
+#endif // !UI_H
