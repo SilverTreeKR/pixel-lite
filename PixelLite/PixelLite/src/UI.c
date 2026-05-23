66f7709864701ea216643e77e53a8fbfdf641cd7
gonghan916 <iopiop4966@gmail.com>
2026-05-23 19:26:21 +0900

    "Style: UI와 main 기능 분리"

--- PixelLite/PixelLite/src/UI.c
+++ PixelLite/PixelLite/src/UI.c
@@ -1,11 +1,7 @@
 ﻿#include <stdio.h>
 #include <windows.h>
 #include <conio.h>
-
-#define MENU_SIZE 7
-#define UP 1
-#define DOWN 2
-#define ENTER 3
+#include "../header/UI.h"
 
 char* menu[MENU_SIZE] = {
     "사진 선택",
@@ -14,6 +10,7 @@ char* menu[MENU_SIZE] = {
     "색상 반전",
     "색상 강조",
     "블러",
+    "CLI",
     "종료"
 };
 
@@ -41,7 +38,7 @@ void displayMenu(int select) {
 
 }
 
-int keyControl()
+int getKey()
 {
     int key = _getch();
 
@@ -64,51 +61,5 @@ int keyControl()
         return ENTER;
     }
 
-    return 0;
-}
-
-int main()
-{
-    int select = 0;
-    int key;
-
-    while (1)
-    {
-        displayMenu(select);
-
-        key = keyControl();
-
-        switch (key)
-        {
-        case UP:
-            select--;
-
-            if (select < 0)
-                select = MENU_SIZE - 1;
-            break;
-
-        case DOWN:
-            select++;
-
-            if (select >= MENU_SIZE)
-                select = 0;
-            break;
-
-        case ENTER:
-            system("cls");
-            printf("선택한 메뉴: %s\n", menu[select]);
-
-            if (select == 6)
-            {
-                printf("\n아무 키나 누르면 종료됩니다.\n");
-                return 0;
-                _getch();
-            }
-
-            printf("\n아무 키나 누르면 메뉴로 돌아갑니다...");
-            _getch();
-            break;
-        }
-    }
     return 0;
 }
---
+++ PixelLite/PixelLite/src/main.c
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include "../header/UI.h"
+
+int main()
+{
+    int select = 0;
+    int key;
+
+    while (1)
+    {
+        displayMenu(select);
+
+        key = getKey();
+
+        switch (key)
+        {
+        case UP:
+            select--;
+
+            if (select < 0)
+                select = MENU_SIZE - 1;
+            break;
+
+        case DOWN:
+            select++;
+
+            if (select >= MENU_SIZE)
+                select = 0;
+            break;
+
+        case ENTER:
+            system("cls");
+            printf("선택한 메뉴: %s\n", menu[select]);
+
+            if (select == 7)
+            {
+                printf("\n아무 키나 누르면 종료됩니다.\n");
+                return 0;
+                _getch();
+            }
+
+            printf("\n아무 키나 누르면 메뉴로 돌아갑니다...");
+            _getch();
+            break;
+        }
+    }
+    return 0;
+}
