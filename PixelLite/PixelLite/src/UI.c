31d522a1d375ec587c0775e488f09780eb023425
gonghan916 <iopiop4966@gmail.com>
2026-05-23 18:38:57 +0900

    "Feat: 메뉴 선택기능 추가"

--- PixelLite/PixelLite/src/UI.c
+++ PixelLite/PixelLite/src/UI.c
@@ -11,15 +11,14 @@ char* menu[MENU_SIZE] = {
     "사진 선택",
     "크로마키",
     "흑백",
-    "색상반전",
+    "색상 반전",
     "색상 강조",
     "블러",
     "종료"
 };
 
 void displayMenu(int select) {
-	system("cls");
-
+    system("cls");
     printf(
         " ______   __     __  __     ______     __            __         __     ______   ______    \n"
         "/\\  == \\ /\\ \\   /\\_\\_\\_\\   /\\  ___\\   /\\ \\          /\\ \\       /\\ \\   /\\__  _\\ /\\  ___\\   \n"
@@ -38,13 +37,13 @@ void displayMenu(int select) {
             printf("  %s   \n", menu[i]);
         }
     }
-    printf("=== 메뉴 선택 ===\n\n");
+    printf("\n(↑ ↓ 이동 / ENTER 선택)\n\n");
 
 }
 
 int keyControl()
 {
-    int key = getch();
+    int key = _getch();
 
     if (key == 224)
     {
@@ -68,3 +67,48 @@ int keyControl()
     return 0;
 }
 
+int main()
+{
+    int select = 0;
+    int key;
+
+    while (1)
+    {
+        displayMenu(select);
+
+        key = keyControl();
+
+        switch (key)
+        {
+        case UP:
+            select--;
+
+            if (select < 0)
+                select = MENU_SIZE - 1;
+            break;
+
+        case DOWN:
+            select++;
+
+            if (select >= MENU_SIZE)
+                select = 0;
+            break;
+
+        case ENTER:
+            system("cls");
+            printf("선택한 메뉴: %s\n", menu[select]);
+
+            if (select == 6)
+            {
+                printf("\n아무 키나 누르면 종료됩니다.\n");
+                return 0;
+                _getch();
+            }
+
+            printf("\n아무 키나 누르면 메뉴로 돌아갑니다...");
+            _getch();
+            break;
+        }
+    }
+    return 0;
+}
