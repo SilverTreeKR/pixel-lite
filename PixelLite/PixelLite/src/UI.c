3e8a58877e78737053c844d889d777c2625ad775
gonghan916 <iopiop4966@gmail.com>
2026-05-23 17:25:49 +0900

    Chores: 메뉴 선택창

--- PixelLite/PixelLite/src/UI.c
+++ PixelLite/PixelLite/src/UI.c
@@ -14,7 +14,7 @@ char* menu[MENU_SIZE] = {
     "종료"
 };
 
-void displayMenu() {
+void displayMenu(int select) {
 	system("cls");
 
     printf(
@@ -22,60 +22,19 @@ void displayMenu() {
         "/\\  == \\ /\\ \\   /\\_\\_\\_\\   /\\  ___\\   /\\ \\          /\\ \\       /\\ \\   /\\__  _\\ /\\  ___\\   \n"
         "\\ \\  _-/ \\ \\ \\  \\/_/\\_\\/_  \\ \\  __\\   \\ \\ \\____     \\ \\ \\____  \\ \\ \\  \\/_/\\ \\/ \\ \\  __\\   \n"
         " \\ \\_\\    \\ \\_\\   /\\_\\/\\_\\  \\ \\_____\\  \\ \\_____\\     \\ \\_____\\  \\ \\_\\    \\ \\_\\  \\ \\_____\\ \n"
-        "  \\/_/     \\/_/   \\/_/\\/_/   \\/_____/   \\/_____/      \\/_____/   \\/_/     \\/_/   \\/_____/ \n"
+        "  \\/_/     \\/_/   \\/_/\\/_/   \\/_____/   \\/_____/      \\/_____/   \\/_/     \\/_/   \\/_____/ \n\n"
     );
+    
+    printf("=== 메뉴 선택 ===\n\n");
 
-    printf("******* 메뉴 선택 *******\n");
     for (int i = 0; i < MENU_SIZE; i++) {
-		printf("%d. %s\n", i + 1, menu[i]);
-	}
-	printf("선택: ");
-}
-
-int main() {
-    int select = 0;
-    int key;
-
-    while (1) {
-        displayMenu(select);
-
-        key = _getch();
-
-        // 방향키 입력 처리
-        if (key == 224) {
-            key = _getch();
-
-            // 위 방향키
-            if (key == 72) {
-                select--;
-
-                if (select < 0)
-                    select = MENU_SIZE - 1;
-            }
-
-            // 아래 방향키
-            else if (key == 80) {
-                select++;
-
-                if (select >= MENU_SIZE)
-                    select = 0;
-            }
+        if (i == select) {
+            printf(" > %s <  \n", menu[i]);
         }
-
-        // Enter 키
-        else if (key == 13) {
-            system("cls");
-            printf("선택한 메뉴: %s\n", menu[select]);
-
-            if (select == 3) {
-                printf("프로그램 종료\n");
-                break;
-            }
-
-            printf("\n아무 키나 누르면 메뉴로 돌아갑니다...");
-            _getch();
+        else {
+            printf("  %s   \n", menu[i]);
         }
     }
+    printf("=== 메뉴 선택 ===\n\n");
 
-    return 0;
 }
