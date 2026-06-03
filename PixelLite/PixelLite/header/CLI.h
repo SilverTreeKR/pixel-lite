8b3d6a0742b345fccf7776708a5531f5a73f0e3c
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-06-03 18:37:11 +0900

    feat: header

--- PixelLite/PixelLite/header/CLI.h
+++ PixelLite/PixelLite/header/CLI.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef CLI_H
+#define CLI_H
+
+typedef void (*CommandFunc)(char*);
+
+typedef struct {
+    char name[20];
+    CommandFunc func;
+} Command;
+
+// 등록
+void registerCommand(char* name, CommandFunc func);
+void registerAll(Command commands[], int n);
+
+// 실행
+void executeCommand(char* name, char* args);
+
+// CLI 시작
+void runCLI(void);
+
+#endif
