969ce7435fa9270ac0d17ea029822e4497476b29
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-06-03 18:26:32 +0900

    feat: registerCommand and registry

--- PixelLite/PixelLite/src/CLI.c
+++ PixelLite/PixelLite/src/CLI.c
@@ -12,3 +12,21 @@ typedef struct {
     char name[20];
     CommandFunc func;
 } Command;
+
+// Registry
+Command registry[MAX_COMMANDS];
+int count = 0;
+
+// =========================
+// Registry 관련 함수
+// =========================
+
+void registerCommand(char* name, CommandFunc func)
+{
+    if (count >= MAX_COMMANDS)
+        return;
+
+    strcpy(registry[count].name, name);
+    registry[count].func = func;
+    count++;
+}
