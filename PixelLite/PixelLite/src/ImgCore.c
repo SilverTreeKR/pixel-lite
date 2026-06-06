00f49bb5ddb42fd04208967475aeb16ea577c11c
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-06-06 19:25:36 +0900

    feat: getExtension function

--- PixelLite/PixelLite/src/ImgCore.c
+++ PixelLite/PixelLite/src/ImgCore.c
@@ -40,3 +40,33 @@ double colorDistance(int r1, int g1, int b1,
     return sqrt((double)(dr * dr + dg * dg + db * db));
 }
 
+// 확장자 추출 후 소문자로 변환
+// 예) "C:\photo\input.PNG" → "png"
+void getExtension(const char* path, char* ext)
+{
+    // 경로 문자열에서 마지막 "." 위치 찾기
+    const char* dot = strrchr(path, '.');
+    // "." 이 없으면 확장자가 없는 파일
+    if (dot == NULL)
+    {
+        // 빈 문자열 저장
+        ext[0] = '\0';
+        return;
+    }
+
+    // 확장자 저장용 인덱스
+    int i = 0;
+
+    dot++; // '.' 이후의 문자 읽기 시작
+   
+	// 문자열 끝까지 또는 최대 15자까지 확장자 추출
+    while (dot[i] != '\0' && i < 15)
+    {
+        // 대문자를 소문자로 변환하여 저장
+        ext[i] = (char)tolower((unsigned char)dot[i]);
+        // 다음 문자 이동
+        i++;
+    }
+    // 문자열 종료 문자 추가
+    ext[i] = '\0';
+}
